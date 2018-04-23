3e2672c52a837a6d91ba480d31e495fb030a8c44
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CustomEdit/GridRowData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomEdit
+{
+    public class GridRowData
+    {
+        public string Name { get; set; }
+        public string List { get; set; }
+    }
+}
---
+++ VB/CustomEdit/GridRowData.vb
@@ -0,0 +1,26 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+
+Namespace CustomEdit
+	Public Class GridRowData
+		Private privateName As String
+		Public Property Name() As String
+			Get
+				Return privateName
+			End Get
+			Set(ByVal value As String)
+				privateName = value
+			End Set
+		End Property
+		Private privateList As String
+		Public Property List() As String
+			Get
+				Return privateList
+			End Get
+			Set(ByVal value As String)
+				privateList = value
+			End Set
+		End Property
+	End Class
+End Namespace
